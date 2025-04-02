152deffac4438495a1b0df701aefad07dfff8986
yeomigie <yeo@njit.edu>
2025-04-02 09:03:03 +0500

    Update docker_compose.py

--- docker_compose.py
+++ docker_compose.py
@@ -4,7 +4,7 @@ services:
   qr-generator:
     build: .
     environment:
-      - QR_DATA_URL=https://github.com/kaw393939
+      - QR_DATA_URL=https://github.com/yeomigie
       - QR_CODE_DIR=/app/qr_codes
       - QR_CODE_FILENAME=github_qr.png
       - FILL_COLOR=black
